75581e6e4ded0af69e60a5898ed4e7b749b72548
rleippe <rleippe@gmail.com>
2018-07-14 16:38:13 -0400

    Revert "Doing the pre-coffee thing"
    This reverts commit d61c1da72c6cddf30c13d84f65f507598ce6f97b.
    I done a bad thing.

--- new_script.rb
+++ new_script.rb
@@ -1,12 +1,1 @@
 # ruby new_script.rb
-# Pre-coffee additions to new script.
-#
-Some stuff
-  for foo in bar
-    if kibbel
-      while kibbel < bits
-        bark
-        # need to increment bits or decrement kibbel
-        # but I need coffee, do it later.
-      end	
-    
