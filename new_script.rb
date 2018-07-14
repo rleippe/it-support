d61c1da72c6cddf30c13d84f65f507598ce6f97b
rleippe <rleippe@gmail.com>
2018-07-14 16:35:15 -0400

    Doing the pre-coffee thing

--- new_script.rb
+++ new_script.rb
@@ -1,1 +1,12 @@
 # ruby new_script.rb
+# Pre-coffee additions to new script.
+#
+Some stuff
+  for foo in bar
+    if kibbel
+      while kibbel < bits
+        bark
+        # need to increment bits or decrement kibbel
+        # but I need coffee, do it later.
+      end	
+    
